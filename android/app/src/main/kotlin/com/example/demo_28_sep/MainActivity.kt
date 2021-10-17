2485e38c4b4a820d396283a9cddee0886bd8b196
Muhammad Nurul Momen <mdnmomen@gmail.com>
2021-10-17 17:08:04 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/demo_28_sep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_28_sep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Ronaldo"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+
+        ),
+      ),
+    ),
+  );
+}
